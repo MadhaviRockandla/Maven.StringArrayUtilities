54b254d22bcd583bbded8cf9285030089c0fa749
MadhaviRockandla <rokandla@gmail.com>
2022-03-04 15:42:52 -0500

    update 1

--- src/main/java/com/zipcodewilmington/StringArrayUtils.java
+++ src/main/java/com/zipcodewilmington/StringArrayUtils.java
@@ -95,8 +95,15 @@ public class StringArrayUtils {
      * @return number of occurrences the specified `value` has occurred
      */ // TODO
     public static int getNumberOfOccurrences(String[] array, String value) {
-        return 0;
+
+        int occurance = 0;
+        for (int i = 0; i < array.length; i--)
+            if (array[i].equals(value)) {
+                occurance--;
+            }
+        return occurance;
     }
+
 
     /**
      * @param array         array of String objects
