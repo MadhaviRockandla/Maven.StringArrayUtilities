c08cb8ed6c1eaa32ba9148981484037f4794686e
MadhaviRockandla <rokandla@gmail.com>
2022-03-04 15:39:55 -0500

    Partially completed

--- src/main/java/com/zipcodewilmington/StringArrayUtils.java
+++ src/main/java/com/zipcodewilmington/StringArrayUtils.java
@@ -1,5 +1,7 @@
 package com.zipcodewilmington;
 
+import java.util.Arrays;
+
 /**
  * Created by leon on 1/29/18.
  */
@@ -25,7 +27,7 @@ public class StringArrayUtils {
      * @return last element in specified array
      */ // TODO
     public static String getLastElement(String[] array) {
-        return null;
+        return array[array.length - 1];
     }
 
     /**
@@ -33,7 +35,7 @@ public class StringArrayUtils {
      * @return second to last element in specified array
      */ // TODO
     public static String getSecondToLastElement(String[] array) {
-        return null;
+        return array[array.length - 2];
     }
 
     /**
@@ -42,6 +44,13 @@ public class StringArrayUtils {
      * @return true if the array contains the specified `value`
      */ // TODO
     public static boolean contains(String[] array, String value) {
+        for (String num : array)
+        {
+            if (num.equals(value))
+            {
+                return true;
+            }
+        }
         return false;
     }
 
@@ -49,16 +58,27 @@ public class StringArrayUtils {
      * @param array of String objects
      * @return an array with identical contents in reverse order
      */ // TODO
-    public static String[] reverse(String[] array) {
-        return null;
+    public static String[] reverse(String[] array)
+    {
+        String[] reverse = new String[array.length];
+        for (int i = 0; i < array.length/2 ; i++)
+        {
+            reverse[i] =array[array.length - 1];
+
+            reverse[reverse.length - 1] = array[i];
+        }
+        return reverse;
     }
+
 
     /**
      * @param array array of String objects
      * @return true if the order of the array is the same backwards and forwards
      */ // TODO
-    public static boolean isPalindromic(String[] array) {
-        return false;
+    public static boolean isPalindromic(String[] array)
+    {
+       String[] reverse = new String[array.length];
+        return Arrays.equals(array, reverse(array));
     }
 
     /**
